0063d332fa057d370e92d39d2b5fbd137211c359
314pie <ian121363@gmail.com>
2019-06-23 19:02:10 +0800

    Fixes issue with nil exception

--- FirebaseStarterApp/FirebaseStarterApp/ViewControllers/NoteEditorViewController.swift
+++ FirebaseStarterApp/FirebaseStarterApp/ViewControllers/NoteEditorViewController.swift
@@ -12,17 +12,31 @@ class NoteEditorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        EditingText.text = NoteText
     }
     
     var NotesID : String = ""
+    var NoteText : String = ""
+    @IBOutlet weak var EditingText: UITextView!
     @IBAction func OnDoneEditing(_ sender: Any) {
         let db = Firestore.firestore()
         let userUID = FirebaseAuthManager.Singleton.UserUID
-       
-        let dict : [String:Any] = ["NoteContext":  "Test Note"]
-        db.collection("users").document(userUID).collection("Notes").document("CurrentTime").setData(dict)
+        NoteText = EditingText.text
+        
+        let lines = NoteText.components(separatedBy: "\n")
+        let titleText = lines.indices.contains(0) ? lines[0]:NoteText
+        
+        let dict : [String:Any] = ["Title":titleText,
+                                   "Context":  NoteText]
+        if(NotesID == ""){
+            //Using current time as note ID
+            let date = Date()
+            let format = DateFormatter()
+            format.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            NotesID = format.string(from: date)
+        }
+        
+        db.collection("users").document(userUID).collection("Notes").document(NotesID).setData(dict)
         dismiss(animated: true, completion: nil)
     }
 }
--- FirebaseStarterApp/FirebaseStarterApp/ViewControllers/NotesViewController.swift
+++ FirebaseStarterApp/FirebaseStarterApp/ViewControllers/NotesViewController.swift
@@ -7,24 +7,61 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class NotesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    class NoteCellData{
+        init(title:String,id:String,context:String) {
+            self.Title = title
+            self.ID = id
+            self.Context = context
+        }
+        var Title : String
+        var ID : String
+        var Context : String
+    }
+    
     @IBOutlet weak var NotesTableView: UITableView!
+    
+    var Notes : Array<NoteCellData> = []
+    
+    func fetchNotes() {
+        let db = Firestore.firestore()
+        let userUID = FirebaseAuthManager.Singleton.UserUID
+        
+        db.collection("users").document(userUID).collection("Notes").getDocuments { (querySnapshot, error) in
+            if let querySnapshot = querySnapshot {
+                self.Notes.removeAll()
+                for document in querySnapshot.documents {
+                    
+                    self.Notes.append(NoteCellData(
+                        title: document.data()["Title"] as! String,
+                        id: document.documentID,
+                        context: document.data()["Context"] as! String ))
+                    self.NotesTableView.reloadData()
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        fetchNotes()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 2
+        return Notes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell") as!NoteTableViewCell
-        cell.setTitle(titleText: "test: "+String(indexPath.row))
+        cell.setTitle(titleText: Notes[indexPath.row].Title)
         return cell
     }
     
@@ -33,6 +70,17 @@ class NotesViewController: UIViewController,UITableViewDelegate,UITableViewDataS
         print("row: \(indexPath.row)")
         
         if let controller = storyboard?.instantiateViewController(withIdentifier: "NoteEditor") {
+            (controller as! NoteEditorViewController).NotesID = Notes[indexPath.row].ID
+            (controller as! NoteEditorViewController).NoteText = Notes[indexPath.row].Context
+            //(controller as! NoteEditorViewController).EditingText.text = Notes[indexPath.row].Context
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func onAddNoteClicked(_ sender: Any) {
+        if let controller = storyboard?.instantiateViewController(withIdentifier: "NoteEditor") {
+            (controller as! NoteEditorViewController).NotesID = ""
+            (controller as! NoteEditorViewController).NoteText = ""
             present(controller, animated: true, completion: nil)
         }
     }
