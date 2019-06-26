62e0f7e34422efc53d2dff14bb7d119d3aa4ff8e
314pie <ian121363@gmail.com>
2019-06-27 00:54:38 +0800

    Add local saving to profile picture

--- FirebaseStarterApp/FirebaseStarterApp/ViewControllers/ProfileViewController.swift
+++ FirebaseStarterApp/FirebaseStarterApp/ViewControllers/ProfileViewController.swift
@@ -12,7 +12,7 @@ class ProfileViewController: UIViewController , UIImagePickerControllerDelegate,
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getProfileImage()
         // Do any additional setup after loading the view.
     }
     
@@ -30,5 +30,33 @@ class ProfileViewController: UIViewController , UIImagePickerControllerDelegate,
         let image = info[.originalImage] as? UIImage
         ProfileImage.image = image
         dismiss(animated: true, completion: nil)
+        saveProfileImage()
+    }
+    
+    
+    func saveProfileImage() {
+        let fileManager = FileManager.default
+        let paths = (getDirectoryPath() as NSString).appendingPathComponent("profile.jpg")
+        let image = ProfileImage.image
+        print(paths)
+        let imageData = image?.jpegData(compressionQuality: 0.9)
+        //let imageData = UIImageJPEGRepresentation(image!, 0.5)
+        fileManager.createFile(atPath: paths as String, contents: imageData, attributes: nil)
+    }
+    
+    func getDirectoryPath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    func getProfileImage(){
+        let fileManager = FileManager.default
+        let imagePath = (self.getDirectoryPath() as NSString).appendingPathComponent("profile.jpg")
+        if fileManager.fileExists(atPath: imagePath){
+            ProfileImage.image = UIImage(contentsOfFile: imagePath)
+        } else {
+            print("No Image")
+        }
     }
 }
